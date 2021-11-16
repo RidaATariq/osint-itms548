d6fcfc4d464c06ddcc169059b7f3dc538742dfef
dnov09 <dngige9@gmail.com>
2021-11-15 22:48:05 -0600

    added credential file

--- api_test.py
+++ api_test.py
@@ -1,8 +1,8 @@
 import twitch
-import config
+import xconfig
 
-client = twitch.TwitchHelix(client_id=config.client_id,
-                            client_secret=config.client_secret)
+client = twitch.TwitchHelix(client_id=xconfig.client_id,
+                            client_secret=xconfig.client_secret)
 
 client.get_oauth()
 print(client.get_streams())
---
+++ config.py
@@ -0,0 +1,3 @@
+# [Credentials]
+client_id = 'Enter your client id'
+client_secret = 'Enter your client secret'
