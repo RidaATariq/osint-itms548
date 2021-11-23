68c3d3259194acd549e036256da9255295738319
dnov09 <dngige9@gmail.com>
2021-11-23 03:17:52 -0600

    changed config details + more - tested code with multiple keywords and it works

--- api_test.py
+++ api_test.py
@@ -1,16 +1,33 @@
+#%%
 import praw
+from praw.models.listing.mixins import subreddit
 import hconfig
 
 reddit = praw.Reddit(
-    client_id=hconfig.client_id,
-    client_secret=hconfig.client_secret,
-    user_agent=hconfig.user_agent,
-    username=hconfig.username,
-    password=hconfig.password,
+    client_id=hconfig.c_id,
+    client_secret=hconfig.secret,
+    user_agent=hconfig.agent,
+    username=hconfig.usr,
+    password=hconfig.pwd,
 )
-reddit.validate_on_submit = True # needed to avoid error
+reddit.validate_on_submit = True  # needed to avoid error
 
 
-# Output score for the first 256 items on the frontpage
-for submission in reddit.front.hot(limit=20):
-    print(submission)
+subreddit = reddit.subreddit('all')
+posts = {"posts": []}
+for submisson in subreddit.search("(cryptocurrency AND ransomware AND hack)",
+                                  limit=10,
+                                  time_filter="all"):
+    posts["posts"].append({
+        "subreddit": submisson.subreddit,
+        "title": submisson.title,
+        "num_comments": submisson.num_comments,
+        "time_created": submisson.created_utc,
+        "author": submisson.author,
+        "post_id": submisson.id
+    })
+
+# print(posts)
+
+
+# %%
