e25414dfdd8cacb3dd7e1db789988e8447e8dab4
dnov09 <dngige9@gmail.com>
2021-11-22 23:15:28 -0600

    basic api calls working on praw - pulled straight from their docs

--- api_test.py
+++ api_test.py
@@ -1,10 +1,16 @@
 import praw
-import config
+import hconfig
 
 reddit = praw.Reddit(
-    client_id=config.client_id,
-    client_secret=config.client_secret,
-    user_agent=config.user_agent,
-    username=config.username,
-    password=config.password,
+    client_id=hconfig.client_id,
+    client_secret=hconfig.client_secret,
+    user_agent=hconfig.user_agent,
+    username=hconfig.username,
+    password=hconfig.password,
 )
+reddit.validate_on_submit = True # needed to avoid error
+
+
+# Output score for the first 256 items on the frontpage
+for submission in reddit.front.hot(limit=20):
+    print(submission)
