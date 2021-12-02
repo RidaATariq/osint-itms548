e618cf313f5fb840fb23c6b01d13c14575f9df10
rtariq <rtariq@hawk.iit.edu>
2021-12-02 08:43:51 -0700

    Pulled author id and saved to the CSV file

--- twitter_api.py
+++ twitter_api.py
@@ -4,13 +4,16 @@ import json
 import pandas as pd
 
 def twitter_client():
-    client = tweepy.Client(bearer_token=hconfig.bearer_token,
-                           consumer_key=hconfig.consumer_key, consumer_secret=hconfig.consumer_secret, access_token=hconfig.access_token, access_token_secret=hconfig.access_secret)
+    client = tweepy.Client(bearer_token=hconfig.bearer_token,consumer_key=hconfig.consumer_key,consumer_secret=hconfig.consumer_secret,
+                           access_token=hconfig.access_token, access_token_secret=hconfig.access_secret)
     return client
 
 def search_tweets(keywords):
     client = twitter_client()
-    tweets = client.search_recent_tweets(query=keywords, max_results=10)
+    tweets = client.search_recent_tweets(query=keywords, max_results=10, expansions='author_id')
+
+
+    # print(tweets)
 
     data = tweets.data
     results = pd.DataFrame(columns = ['id','text'])
@@ -20,6 +23,8 @@ def search_tweets(keywords):
             obj = {}
             obj['id'] = tweet.id
             obj['text'] = tweet.text
+            obj['author_id'] = tweet.author_id
+            # obj['username'] = tweet.entities.mentions.username
             results = results.append(obj, ignore_index = True)
     else:
         return ''
