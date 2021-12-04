e7f8cbd5cdccc734be50f5a151cef47c8e6e9231
Hareem Akram <hakram@hawk.iit.edu>
2021-12-04 01:06:15 -0600

    cleaned visualization code
    - removed unnecessary comments and code

--- plots.py
+++ plots.py
@@ -6,6 +6,7 @@ y = []
 y_num = []
 x_filtered = []
 
+# open csv file generated using
 with open('reddit_dataset.csv','r') as reddit_csv:
 	reddit_data = csv.reader(reddit_csv, delimiter = ',')
 	next(reddit_csv)
@@ -13,30 +14,20 @@ with open('reddit_dataset.csv','r') as reddit_csv:
 	for row in reddit_data:
 		x.append(row[1])
 		y.append(row[4])
-#print(y)
-#print(y)
-
+		
+# convert all values in list y from str to int
 for i in y:
     j=int(i)
     y_num.append(j)
 
-#y_num=[int(y) for i in y]
-#print(y_num)
-
-#y_num.sort()
-
+# convert x and y lists to a dictionary
 myDict = {x[i]: y_num[i] for i in range(len(x))}
-#print(myDict)
 myList = myDict.items()
 myList = sorted(myList)
 x, y = zip(*myList)
 
-##plt.bar(x, y_num, color = 'g', width = 0.72)
-##plt.xlabel('Authors')
-##plt.ylabel('Number of Comments')
-##plt.title('Number of Comments per Author')
-##plt.ylim([10, 100])
-##plt.show()
-
+# creating line graph using dictionary
+plt.xlabel('Authors')
+plt.ylabel('Number of Comments')
 plt.plot(x, y)
 plt.show()
