3a8784f9b8a9ec51711e01f876f0fb305060f8ce
Hareem Akram <hakram@hawk.iit.edu>
2021-12-04 02:21:22 -0600

    removed irrelevant data

--- plots.py
+++ plots.py
@@ -12,8 +12,8 @@ with open('reddit_dataset.csv','r') as reddit_csv:
 	next(reddit_csv)
 	
 	for row in reddit_data:
-		x.append(row[1])
-		y.append(row[4])
+		reddit_x.append(row[1])
+		reddit_y.append(row[6])
 		
 # convert all values in list y from str to int
 for i in y:
@@ -21,16 +21,19 @@ for i in y:
     y_num.append(j)
 
 # convert x and y lists to a dictionary
-myDict = {x[i]: y_num[i] for i in range(len(x))}
-myList = myDict.items()
-myList = sorted(myList)
-x, y = zip(*myList)
+##redditDict = {reddit_x[i]: reddit_y_num[i] for i in range(len(reddit_x))}
+##redditList = redditDict.items()
+##redditList = sorted(redditList)
+##x, y = zip(*redditList)
 
 # creating line graph using dictionary
-plt.xlabel('Authors')
-plt.ylabel('Number of Comments')
-plt.plot(x, y)
-plt.show()##        datetime_obj = datetime.datetime.strptime(i, '%Y-%m-%d %H:%M:%S.%f')
+##plt.xlabel('Authors')
+##plt.ylabel('Number of Comments')
+##plt.plot(x, y)
+##plt.show()
+
+##for i in reddit_y:
+##        datetime_obj = datetime.datetime.strptime(i, '%Y-%m-%d %H:%M:%S.%f')
 ##        reddit_y_num.append(datetime_obj)
 ##
 ##print(reddit_y_num)
