4edcd671c02db5e69d30aa3ef33428fc1a7a2616
rtariq <rtariq@hawk.iit.edu>
2021-12-05 02:59:49 -0700

    Added Tweet creation date and time without any errors.

--- twitter_api.py
+++ twitter_api.py
@@ -3,7 +3,6 @@ import hconfig
 import json
 import pandas as pd
 
-
 def twitter_client():
     client = tweepy.Client(bearer_token=hconfig.bearer_token,consumer_key=hconfig.consumer_key,consumer_secret=hconfig.consumer_secret,
                            access_token=hconfig.access_token, access_token_secret=hconfig.access_secret)
@@ -11,7 +10,7 @@ def twitter_client():
 
 def search_tweets(keywords):
     client = twitter_client()
-    tweets = client.search_recent_tweets(query=keywords, max_results=10, expansions='author_id',tweet_fields="entities")
+    tweets = client.search_recent_tweets(query=keywords, max_results=10, expansions='author_id',tweet_fields=["entities","created_at"])
 
     data = tweets.data
     results = pd.DataFrame(columns = ['id','text'])
@@ -22,7 +21,9 @@ def search_tweets(keywords):
             obj['id'] = tweet.id
             obj['text'] = tweet.text
             obj['author_id'] = tweet.author_id
-            obj['entities'] = tweet.entities
+            #obj['entities'] = tweet.entities
+            obj['date_time'] = tweet.created_at
+
             #This try except block tries to pull usernames
             try:
                 obj['username'] = tweet.entities['mentions'][0]['username']
