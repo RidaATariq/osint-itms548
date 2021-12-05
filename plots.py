2cbaa53114075c314cd84364d07dedb4944f8c32
rtariq <rtariq@hawk.iit.edu>
2021-12-05 04:45:58 -0700

    Added Twitter data plot to plots.py and made some minor changes in rest of the code.

--- plots.py
+++ plots.py
@@ -1,6 +1,8 @@
 import matplotlib.pylab as plt
 import csv
 from datetime import datetime, timezone
+import pandas as pd
+import seaborn as sns
 
 reddit_x = []
 reddit_y = []
@@ -13,11 +15,11 @@ testList = []
 with open('reddit_dataset.csv','r') as reddit_csv:
 	reddit_data = csv.reader(reddit_csv, delimiter = ',')
 	next(reddit_csv)
-	
+
 	for row in reddit_data:
 		reddit_x.append(row[1])
 		reddit_y.append(row[6])
-		
+
 # convert all values in list y from str to int
 for i in reddit_y:
     j=int(float(i))
@@ -54,7 +56,21 @@ for i in reddit_y_num:
 for i in range(len(dateList)):
         testList.append(i)
 
-plt.xlabel('posts')
-plt.ylabel('date')
-plt.plot(testList, dateList)
+plt.title("Reddit posts versus date/time")
+plt.ylabel('posts')
+plt.xlabel('date')
+plt.plot(dateList,testList)
+plt.show()
+
+# Twitter plot
+
+Tweet_data = pd.read_csv("Twitter_dataset.csv")
+Tweet_data['date_time'] = pd.to_datetime(Tweet_data['date_time'])
+print(Tweet_data.head())
+print(Tweet_data.columns)
+plt.figure()
+sns.scatterplot(x=Tweet_data['date_time'] ,y=Tweet_data.index)
+plt.ylabel('Tweets')
+plt.title("Tweets versus date/time")
+plt.xlabel("date/time")
 plt.show()
