bd9455493ae874d2b28fc2969637a17388bfcf8b
rtariq <rtariq@hawk.iit.edu>
2021-12-06 10:28:22 -0700

    Added GUI for Reddit in the Dashboard

--- GUI_Twitter_Reddit.py
+++ GUI_Twitter_Reddit.py
@@ -12,20 +12,52 @@ from reddit_api import get_data
 
 class Dashboard():
     def __init__(self):
+        
+        def t_compute():
+            num_tweets = int(tweets1.get())
+            if num_tweets >= 0:
+                tweets_dataset = search_tweets("bitcoin ransomware",num_tweets)
+                tweets_dataset.to_csv("Twitter_dataset.csv", index = None)
+                t_status.config(text='%d tweets pulled & saved to Twitter CSV' %num_tweets)
+            else:
+                error = Label(window, text='Invalid input for No. of Tweets.', background='#FFFFCB')
+                error.grid(column=1, row=11)
+                return
+        
+        def r_compute():
+            num_tweets = int(tweets1.get())
+            if num_tweets >= 0:
+                reddit_dataset = get_data("bitcoin ransomware")
+            else:
+                error = Label(window, text='Invalid input for No. of Tweets.', background='#FFFFCB')
+                error.grid(column=1, row=11)
+                return
+        
         window = Tk()
         window.title('Dashboard')
         window.geometry('1200x195')
 
         window.configure(background='#FFFFFF')
-        blue_color = '#DFF3F8'
-        start = Label(window, text='Enter the number of posts you want to pull and hit, Click Me button :', background='#FFFFFF')
+        start = Label(window, text='Enter the number of tweets/posts you want to pull and hit, Click button:', background='#FFFFFF', font='Helvetica 15 bold')
         start.grid(column=1, row=0)
         gap = Label(window, text='', background='#FFFFFF').grid(column=0, row=1)
         gap = Label(window, text='', background='#FFFFFF').grid(column=1, row=1)
         gap = Label(window, text='', background='#FFFFFF').grid(column=3, row=1)
         gap = Label(window, text='', background='#FFFFFF').grid(column=1, row=3)
+        gap = Label(window, text='', background='#FFFFFF').grid(column=0, row=5)
+        gap = Label(window, text='', background='#FFFFFF').grid(column=1, row=5)
+        gap = Label(window, text='', background='#FFFFFF').grid(column=2, row=5)
+        gap = Label(window, text='', background='#FFFFFF').grid(column=3, row=5)
+        gap = Label(window, text='', background='#FFFFFF').grid(column=1, row=7)
+        
+        # =============================================================================
+        # Twitter IO
+        # =============================================================================
+        blue_color = '#DFF3F8'
+        reddit_color = '#FFF3F8'
 
-        tweet_label = Label(window, text='No. of Tweets (10-100): ', background=blue_color).grid(column=0, row=2)
+
+        tweet_label = Label(window, text='  No. of Tweets (10-100): ', background=blue_color).grid(column=0, row=2)
 
         tweets1 = Entry(window, width=12, background=blue_color)
         tweets1.grid(column=1, row=2)       
@@ -35,22 +67,30 @@ class Dashboard():
         t_status = Label(window, text='', background=blue_color)
         t_status.grid(column=3, row=2)
         
-        def compute():
-            num_tweets = int(tweets1.get())
-            if num_tweets >= 0:
-                tweets_dataset = search_tweets("bitcoin ransomware",num_tweets)
-                tweets_dataset.to_csv("Twitter_dataset.csv", index = None)
-                t_status.config(text='%d tweets pulled & saved to Twitter CSV' %num_tweets)
-                reddit_dataset = get_data("bitcoin ransomware")
-            else:
-                error = Label(window, text='Invalid input for No. of Tweets.', background='#FFFFCB')
-                error.grid(column=1, row=11)
-                return
-     
-        
-        button_twitter = Button(window, text='Twitter Pull', command=compute,
-                                bg=blue_color, activebackground=blue_color,
+        button_twitter = Button(window, text='Twitter Pull', command=t_compute,
+                                bg=blue_color, activebackground=reddit_color,
                                 height='1', width='15').grid(column=1, row=4)
+        
+        # =============================================================================
+        # Reddit IO
+        # =============================================================================
+
+        reddit_label = Label(window, text='  No. of Posts (10-100): ', background=reddit_color).grid(column=0, row=6)
+
+        reddits1 = Entry(window, width=12, background=reddit_color)
+        reddits1.grid(column=1, row=6)       
+        
+        r_label = Label(window, text='Reddit Data status: ', background=reddit_color).grid(column=2, row=6)
+        
+        r_status = Label(window, text='', background=reddit_color)
+        r_status.grid(column=3, row=6)
+
+        button_reddit = Button(window, text='Reddit Pull', command=r_compute,
+                                bg=reddit_color, activebackground=blue_color,
+                                height='1', width='15').grid(column=1, row=8)
+        
+        
+        
         window.mainloop()
 
 # Start of Code
